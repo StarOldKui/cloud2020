43b317054ca2ae972d159820f25ecc951adba16c
Ruiyan Xia <ruiyanx@student.unimelb.edu.au>
2020-09-17 18:35:06 +0800

    第二章 1.1

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -6,9 +6,9 @@ import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -27,8 +27,11 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
-//    @Resource
-//    private DiscoveryClient discoveryClient;
+    /**
+     * 服务发现
+     */
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
@@ -52,20 +55,21 @@ public class PaymentController {
         }
     }
 
-   /* @GetMapping(value = "/payment/discovery")
+    @GetMapping(value = "/payment/discovery")
     public Object discovery() {
         List<String> services = discoveryClient.getServices();
         for (String element : services) {
             log.info("*****element: " + element);
         }
 
+        // 获取端口信息
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
         for (ServiceInstance instance : instances) {
             log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
         }
 
         return this.discoveryClient;
-    }*/
+    }
 
     @GetMapping(value = "/payment/lb")
     public String getPaymentLB() {
